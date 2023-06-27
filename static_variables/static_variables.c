3b16cc3377ddb98c42aafaf24b61dfd971418ede
Luan Lopes <luan.lopes@clickativo.com.br>
2023-06-26 22:01:16 -0300

    Adding studies about command parameter, function, static variables, type definitions, enum and structures

---
+++ static_variables/static_variables.c
@@ -0,0 +1,30 @@
+int increment_age();
+int incremente_static_age();
+
+int main(void) {
+  // inside function you can initialize a static variable
+  // using the static keyword.
+  // Note that global variables are static by default.
+
+  // A static variable is initialized to 0 if no initial value is specified, and
+  // it retains the value across function calls.
+
+  // Same behaviour as a variable declaration, but every element of the array
+  // will be initialized to 0;
+  static int ages[3];
+}
+
+// This fn will always give us the same return value;
+int increment_age() {
+  int age = 0;
+  age++;
+  return age;
+}
+
+// This fn will always give us the age + 1 as a return value;
+int incremente_static_age() {
+  // Static variables are automatically initialized to 0;
+  static int age;
+  age++;
+  return age;
+}
