f4eb63c34de81b36b2cb2b3c3747cf23d6e1e1f1
Luan Lopes <luan.lopes@clickativo.com.br>
2023-07-03 23:31:16 -0300

    feature: adding study about Makefiles, and adding Makefiles to previous directories

--- 12-static_variables/static_variables.c
+++ 12-static_variables/static_variables.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 int increment_age();
 int incremente_static_age();
 
@@ -12,6 +13,15 @@ int main(void) {
   // Same behaviour as a variable declaration, but every element of the array
   // will be initialized to 0;
   static int ages[3];
+  printf("increment_age: %d\n", increment_age());
+  printf("increment_age: %d\n", increment_age());
+  printf("increment_age: %d\n", increment_age());
+
+  printf("\nNow with static variable\n\n");
+
+  printf("increment_static_age: %d\n", incremente_static_age());
+  printf("increment_static_age: %d\n", incremente_static_age());
+  printf("increment_static_age: %d\n", incremente_static_age());
 }
 
 // This fn will always give us the same return value;
